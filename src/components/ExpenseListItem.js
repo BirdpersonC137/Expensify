5e5db040fad6b5f561c19ac2d6e70bea8b68d0b0
Vlad Plotnikov <vladimir.plotnikov89@gmail.com>
2018-02-23 11:37:49 -0800

    moved remove

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {connect} from 'react-redux';
-import {removeExpense} from '../actions/expenses';
 import moment from 'moment';
 const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => (
     <div>
         <Link to={`/edit/${id}`}><h3>{description}</h3></Link>
         <div>{amount}-{moment(createdAt).format('MMM Do YY')}</div>
-        <button onClick={()=>{
-            dispatch(removeExpense({id}))
-        }}>Remove</button>
     </div>
 )
-export default connect()(ExpenseListItem);
+export default ExpenseListItem;
