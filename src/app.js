f0998a2558bcb4460ffa413ed18f69901c2860aa
Vlad Plotnikov <vladimir.plotnikov89@gmail.com>
2018-03-10 15:08:01 -0800

    started setting up for production

--- src/app.js
+++ src/app.js
@@ -9,17 +9,8 @@ import getVisibleExpenses from './selectors/expenses'
 import 'normalize.css/normalize.css'
 import 'react-dates/lib/css/_datepicker.css';
 import './styles/styles.scss';
-import { setTimeout } from 'timers';
 
 const store = configureStore();
-
-store.dispatch(addExpense({description: 'Water Bill', note: 'Pay for water usage', amount: 700,createdAt: 1000}))
-store.dispatch(addExpense({description: 'Gas Bill', note: 'Pay for gas usage', amount: 80000,createdAt: 1001}))
-store.dispatch(addExpense({description: 'Rent', note: 'Pay for gas usage', amount: 1050,createdAt: 1002}))
-
-// setTimeout(()=>{
-//     store.dispatch(setTextFilter('water'))
-// }, 3000)
 const state = store.getState()
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
 const jsx = (
