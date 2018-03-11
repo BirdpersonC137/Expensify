2ac5cc16771008d17edcb8337847196c82687d05
Vlad Plotnikov <vladimir.plotnikov89@gmail.com>
2018-03-10 17:08:55 -0800

    added formatting modules

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import moment from 'moment';
+import numeral from 'numeral';
 const ExpenseListItem = ({dispatch, id, description, amount, createdAt}) => (
     <div>
         <Link to={`/edit/${id}`}><h3>{description}</h3></Link>
-        <div>{amount}-{moment(createdAt).format('MMM Do YY')}</div>
+        <div>
+        {numeral(amount).format('$0,0.00')}
+        -
+        {moment(createdAt).format('MMMM Do YYYY')}</div>
     </div>
 )
 export default ExpenseListItem;
