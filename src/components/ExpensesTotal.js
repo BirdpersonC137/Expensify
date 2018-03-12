86c6cc346426f1366bcbd072d3342ddaa8feac63
Vlad Plotnikov <vladimir.plotnikov89@gmail.com>
2018-03-12 15:54:39 -0700

    added total feature

---
+++ src/components/ExpensesTotal.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import numeral from 'numeral';
+import selectExpenses from '../selectors/expenses';
+import selectExpensesTotal from  '../selectors/expensesTotal';
+const ExpensesTotal = ({expenseCount, expensesTotal}) => {
+    const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
+    const formattedExpensesTotal = numeral(expensesTotal).format('$0,0.00');
+    return (
+        <div>
+            <h3>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}</h3>
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+    const visibleExpenses = selectExpenses(state.expenses, state.filters);
+    return {
+        expenseCount: visibleExpenses.length,
+        expensesTotal: selectExpensesTotal(visibleExpenses)
+    }
+};
+
+export default connect(mapStateToProps)(ExpensesTotal);
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
-import ExpenseListFilters from './ExpenseListFilters'
+import ExpenseListFilters from './ExpenseListFilters';
+import ExpensesTotal from './ExpensesTotal';
 const Home = () => (
     <div>
+        <ExpensesTotal/>
         <ExpenseListFilters/>
         <ExpenseList/>
     </div>
 )
 
-export default Home
+export default Home;
